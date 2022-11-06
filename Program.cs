4657dd56ed463beace0ab918afc6a6a428705b68
Chaymae Mechouahi <113052518+chaymaeme@users.noreply.github.com>
2022-11-06 20:06:59 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+using System;
 
-namespace Tp2
+public class Examen
 {
-    class Program
+	private DateTime dateexam;
+	private String examde;
+	private DateTime datefin;
+	private int e_nbrequestion;
+	private int e_note;
+	public Examen(DateTime date, String de, DateTime fin,int question , int note)//constructeur
+    {  		
+		dateexam = date;
+		de = examde;
+		datefin = fin;
+		e_nbrequestion = question;
+		e_note = note;
+    }
+	public DateTime Dateexam //setters et getters de dateexam
+	{
+		get { return dateexam ; }
+		set { dateexam = value ; }
+	}
+	/* public void affichedateexam ()
+	 * { Console.WriteLine("Date d'examen est : " +dateexam );}*/
+	public String Examde //setters getters de exam de
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        get { return examde ;}
+        set { examde = value; }
+    }
+	public DateTime Datefin //setters getters datefin
+    {
+		get { return datefin; }
+		set { datefin = value; }
+    } 
+	public int Nbrequestion//setters getters nbrequestion
+    {
+        get { return e_nbrequestion; }
+		set { e_nbrequestion = value; }
+    }
+	public int Note//setters getters note
+    {
+        get { return e_note; }
+        set { e_note = value; }
     }
 }
