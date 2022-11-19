ad51e82f02e2f71f337f4a0180c64903d4e2b3e3
Chaymae Mechouahi <113052518+chaymaeme@users.noreply.github.com>
2022-11-19 18:22:00 +0000

    ops

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 using System;
 
+
+namespace Tp2
+{
+    class ProgramPrincipale
+    {
+        static void Main(string[] args)
+        {
+
+		}
+    }
 public class Examen
 {
 	private DateTime dateexam;
@@ -7,6 +17,14 @@ public class Examen
 	private DateTime datefin;
 	private int e_nbrequestion;
 	private int e_note;
+	private Question question1 = new Question();
+    private Question question2 = new Question();
+    private Question question3 = new Question();
+		//... nbre de question 
+	private CopieEtudiant cp1 = new CopieEtudiant();
+	private CopieEtudiant cp2 = new CopieEtudiant();
+	private CopieEtudiant cp3 = new CopieEtudiant();
+		//...nbre d'étudiant
 	public Examen(DateTime date, String de, DateTime fin,int question , int note)//constructeur
     {  		
 		dateexam = date;
@@ -42,4 +60,63 @@ public class Examen
         get { return e_note; }
         set { e_note = value; }
     }
+}
+	public abstract class Question //plusieurs questions ont les memes attributs  
+	{
+		private int reference;
+		private string txtquestion;
+		private int note;
+		private Reponse reponse;
+	}
+	public class QCM : Question //inheritance
+	{
+		private int nbreproposition;
+		private int nbrepropojuste;		
+	}
+	public class VraiFaux : Question //inheritance
+	{
+		//attribut && méthode
+	}
+	public class QuestionIndirect : Question //inheritance
+	{
+		//attribut  && méthode
+	}
+	public abstract class Reponse //plusieurs reponses ont les memes attributs 
+	{
+		private int note ;
+		private int pourcentage_reponse;
+	}
+	public class PropositionQCM : Reponse
+	{
+		private int reference;//reference de chaque reponse du qcm  comme 1.a. , 1.b. , 1.c.
+	}
+	public class ReponseVF : Reponse
+	{
+		//attributs && méthode 
+	}
+	public class ReponseInd : Reponse
+	{
+		//attributs && méthode
+	}
+	public class CopieEtudiant
+	{
+		private string nom;
+		private ReponseEtudiant reponse = new ReponseEtudiant();
+	}
+	public abstract class ReponseEtudiant
+    {
+		//attributs && méthode
+    }
+    public class ReponseEINd : ReponseEtudiant
+	{
+		//attributs && méthode
+	}
+	public class EVF : ReponseEtudiant
+	{
+		//attributs && méthode
+	}
+	public class QCM : ReponseEtudiant
+	{
+		//attributs && méthode
+	}
 }
